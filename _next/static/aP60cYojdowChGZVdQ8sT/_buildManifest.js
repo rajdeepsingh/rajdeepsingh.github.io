90e4e3fcda50a851958041bc90061a9567a1e796
Rajdeep Singh <rajdeep.singh11@justeattakeaway.com>
2023-05-04 16:48:26 -0600

    hackathon-may-2023-magic-skip-button-1

---
+++ _next/static/aP60cYojdowChGZVdQ8sT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/473-21dd189facfc48e7.js","static/chunks/pages/index-ebf85a6a927fcd29.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
